f9e3a2386d1f692df0c7867470452091684b3ecf
Can Cimenser <scan_cimenser@hotmail.com>
2020-10-27 15:32:48 +0300

    The contact anf ticket update, index and show parts are completed. Please check

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,36 @@
 class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    # creating new contact with the required params
+    @contact.user = current_user
+    # user that contact belongs to is the user with the specified id
+    if @contact.save
+      redirect_to root_path
+      # can be changed after the routes are determined
+    else
+      render :new
+      # paths may change after setting the routes
+    end
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    @contact.user = current_user
+    redirect_to root_path
+    # can be changed after all the routes are settled
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:full_name, :phone_number)
+  end
 end
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,26 @@
 class TicketsController < ApplicationController
+  before_action :ticket_id, only: [:show, :update]
+  def index
+    @tickets = Ticket.all
+  end
+
+  def show; end
+
+  #def edit; end
+
+  def update
+    @ticket.update(ticket_params)
+    redirect_to user_registration_path
+    # redirect path can change after setting the routes
+  end
+
+  private
+
+  def ticket_id
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(:status, :comment)
+  end
 end
