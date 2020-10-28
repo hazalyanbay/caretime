fdd2953f2ca9e56cbcec8357a172f4f04d454831
Can Cimenser <scan_cimenser@hotmail.com>
2020-10-28 15:28:21 +0300

    The tickets index and show views are improved, new features added.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :tickets, only: [ :new, :index, :create, :edit, :update]
+  resources :tickets, only: [ :new, :index, :show, :create, :edit, :update]
   resources :contacts, only: [:new, :create, :edit, :update]
   resources :chatrooms, only: :show do
      resources :messages, only: :create
