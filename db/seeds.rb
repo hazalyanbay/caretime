f6bdd7d771c0131ce5b685573bc497afcc9b662f
Can Cimenser <scan_cimenser@hotmail.com>
2020-10-30 14:33:39 +0300

    diretly going to chatroom

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@
 Chatroom.destroy_all
 Ticket.destroy_all
 User.destroy_all
+
 workerone = User.create!(worker: true, first_name: 'hazal', last_name: 'yanbay', phone_number: '5053678898', address: 'beyoglu', email: 'buse.xezal@gmail.com', password: '555555')
 workertwo = User.create!(worker: true, first_name: 'semyon', last_name: 'kazanstev', phone_number: '464673382', address: 'beyoglu', email: 'semyon@gmail.com', password: '555555')
 workerthree = User.create!(worker: true, first_name: 'can', last_name: 'cimensever', phone_number: '505369009', address: 'beyoglu', email: 'can@gmail.com', password: '555555')
@@ -22,12 +23,13 @@ victimthree = User.create!(worker: false, first_name: 'hazal', last_name: 'buse'
 victimfour = User.create!(worker: false, first_name: 'selin', last_name: 'hürmüz', phone_number: '5053678898', address: 'beyoglu', email: 'selin@gmail.com', password: '555555')
 victimfive = User.create!(worker: false, first_name: 'serpil', last_name: 'demirci', phone_number: '5053678898', address: 'beyoglu', email: 'serpil@gmail.com', password: '555555')
 
-
-
-
 ticketone = Ticket.create!(issue_type: "I'm in danger, I need help!", status: 'solved', comment: "called the police, she is safe now", worker: workerone, victim: victimone)
 tickettwo = Ticket.create!(issue_type: "I want to use hidden live chat", status: 'solved', comment: "we found her a place to stay for tonight", worker: workertwo, victim: victimtwo)
 ticketthree = Ticket.create!(issue_type: "I want to speak to specialist via video chat", status: 'solved', comment: "we talked with her, she decided to move another house", worker: workerthree, victim: victimthree)
+
+chatroomone = Chatroom.create!(name: "Chatroom for ticket #{ticketone.id}", ticket: ticketone)
+chatroomtwo = Chatroom.create!(name: "Chatroom for ticket #{tickettwo.id}", ticket: tickettwo)
+chatroomthree = Chatroom.create!(name: "Chatroom for ticket #{ticketthree.id}", ticket: ticketthree)
 
 contactone = Contact.create(full_name: 'Ahmet Can', phone_number: '5556667788',user: victimone)
 contactone = Contact.create(full_name: 'Ahmet Han', phone_number: '5556667789',user:victimtwo)
